6bba4d988c5662c3b601ba726aaee8c15eddc0df
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-06-28 21:25:52 +0300

    homePage

--- my-app/src/component/Footer.js
+++ my-app/src/component/Footer.js
@@ -4,15 +4,24 @@ import './Footer.css'
 export class Footer extends Component {
   render() {
     return (
-      <div>
-        <Navbar style={{ backgroundColor: 'black' }} className="navbar" >
-          <div className='navbar-container'>
-            <p className='navbar-logo'> CaptureIt </p>
-          </div>
+      <>
+        <title>Untitled</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"></link>
+        <link rel="stylesheet" href="assets/css/style.css"></link>
+        <div class="footer-basic">
 
-        </Navbar>
-
-      </div >
+          <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+          <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Home</a></li>
+            <li class="list-inline-item"><a href="#">Services</a></li>
+            <li class="list-inline-item"><a href="#">About</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+          </ul>
+          <p class="copyright">CaptureIt © 2021</p>
+        </div>
+      </>
     );
   }
 }
--- my-app/src/component/home/InputForm.js
+++ my-app/src/component/home/InputForm.js
@@ -8,7 +8,7 @@ export class InputForm extends Component {
         <Form onSubmit={this.props.getPhoto}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             {/* <Form.Label>Enter Picture Name</Form.Label> */}
-            <Form.Control className="mb-4" type="text" placeholder="Enter Picture Name" name="photo" />
+            <Form.Control style={{ width: '400px', marginTop: ' 50px', height: '55px' }} className="mb-4" type="text" placeholder="Enter Picture Name" name="photo" />
           </Form.Group>
           <button className="mb-5" variant="primary" type="submit">Search</button>
         </Form>
