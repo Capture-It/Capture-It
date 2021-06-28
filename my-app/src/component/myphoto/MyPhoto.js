a9ac8e1e7f3dbfe4033df0e3df3e2d5c652796bd
AbdullahElian1 <abodelian28@gmail.com>
2021-06-28 10:46:41 +0300

    edit comment

--- my-app/src/component/myphoto/MyPhoto.js
+++ my-app/src/component/myphoto/MyPhoto.js
@@ -36,6 +36,9 @@ export class MyPhoto extends Component {
   //this function to render the image from the database that the user choose
   componentDidMount = async () => {
     console.log('heeer');
+    // console.log(this.props.auth0.user);
+    // this.props.auth0.user.picture='https://tse4.mm.bing.net/th?id=OIP.3PLCiUSmRbFhJtkdmY23_AHaEK&pid=Api&P=0&w=283&h=160'
+    // console.log(this.props.auth0.user);
     let email = this.props.auth0.user.email;
     let url = `http://localhost:3010/getphoto?email=${email}`;
     axios.get(url).then((photoResult) => {
