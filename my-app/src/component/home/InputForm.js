2ae702b204097d90cf849bfaa4ea264b9a25275d
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-06-28 10:31:06 +0300

    edited-search

--- my-app/src/component/home/InputForm.js
+++ my-app/src/component/home/InputForm.js
@@ -7,10 +7,10 @@ export class InputForm extends Component {
       <div>
         <Form onSubmit={this.props.getPhoto}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Picture Name</Form.Label>
+            {/* <Form.Label>Enter Picture Name</Form.Label> */}
             <Form.Control className="mb-4" type="text" placeholder="Enter Picture Name" name="photo" />
           </Form.Group>
-          <Button className="mb-5" variant="primary" type="submit">Search</Button>
+          <button className="mb-5" variant="primary" type="submit">Search</button>
         </Form>
 
       </div>
